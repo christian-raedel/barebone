b03726278709e3ffe1591f3e6c5fa0033a2d5268
Christian Rädel <christian.raedel@gmail.com>
2014-07-11 13:04:31 +0200

    added create directory methods to FsActions

--- modules/fs-actions/fs-actions.js
+++ modules/fs-actions/fs-actions.js
@@ -1,5 +1,6 @@
 var _ = require('lodash')
     , q = require('q')
+    , async = require('async')
     , fs = require('fs')
     , Conf = require('../conf');
 
@@ -29,55 +30,97 @@ FsActions.prototype.config = function(config) {
         console.log('Q\'s longStackSupport is ' + (q.longStackSupport ? 'enabled' : 'disabled'));
     });
 
-    conf.load(config);
-    this.conf = conf;
-
+    this.conf = conf.load(config);
     return conf;
 };
 
-FsActions.prototype.mkdir = function(dir) {
-    var exists = q.denodeify(fs.exists)
-        , stat = q.denodeify(fs.stat)
-        , mkdir = q.denodeify(fs.mkdir);
+FsActions.prototype.createDir = function(dir) {
+    var defer = q.defer()
+        , del = this.conf.get('fsDelimiter');
 
-    if (!_.isString(dir)) {
-        throw new Error('invalid directory argument "' + dir + '"!');
+    function create(dir, cb) {
+        if (dir[dir.length - 1] !== del) {
+            dir += del;
+        }
+
+        fs.exists(dir, function(err, exists) {
+            if (err) {
+                return cb(err);
+            }
+
+            if (exists) {
+                return cb(null, true);
+            }
+
+            fs.mkdir(dir, function(err) {
+                if (err) {
+                    return cb(err);
+                }
+
+                return cb(null, true);
+            });
+        });
+    }
+
+    if (_.isString(dir)) {
+        dir = [dir];
+    }
+
+    async.each(dir, function(dir, cb) {
+        var dirs = _(dir).map(dir.split(del), function(dir, idx, dirs) {
+            return dirs.slice(0, idx).join(del);
+        })
+        .filter(dirs, function(dir) {
+            return dir.length === 0 ? false : true;
+        });
+
+        async.each(dirs, create, function(err) {
+            if (err) {
+                return cb(err);
+            }
+
+            return cb(null, true);
+        });
+    }, function(err) {
+        if (err) {
+            defer.reject(err);
+        }
+
+        defer.resolve(true);
+    });
+
+    return defer.promise;
+};
+
+FsActions.prototype.createDirSync = function(dir) {
+    if (!dir) {
+        throw new Error('invalid arguments to creating directories!');
     }
 
     var del = this.conf.get('fsDelimiter');
-    while (dir[dir.length - 1] === del) {
-        dir = dir.substr(0, dir.length - 2);
+
+    if (_.isString(dir)) {
+        dir = [dir];
     }
 
-    console.log('trying to create directory "' + dir + '"...');
-    var dirs = dir.split(del)
-        , chain = q.fcall(function() {});
-    _.forEach(dirs, function(dir, idx, dirs) {
-        if (idx > 0) {
-            dir = dirs.slice(0, idx + 1).join(del);
-            console.log(dir);
-
-            function link() {
-                return exists(dir).then(function(exists) {
-                    var defer = q.defer();
-
-                    if (!exists) {
-                        mkdir(dir).then(function() {
-                            console.log('directory "' + dir + '" created');
-                            defer.resolve(true);
-                        })
-                        .catch(defer.reject);
-                    } else {
-                        defer.resolve(false);
-                    }
-
-                    return defer.promise;
-                });
-            }
-
-            chain = chain.then(link);
+    _.forEach(dir, function(dir) {
+        if (dir[dir.length - 1] !== del) {
+            dir += del;
         }
+
+        var parts = _.map(dir.split(del), function(dir, idx, dirs) {
+            return dirs.slice(0, idx).join(del);
+        });
+        parts = _.filter(parts, function(dir) {
+            return dir.length === 0 ? false : true;
+        });
+
+        _.forEach(parts, function(part) {
+            if (!fs.existsSync(part)) {
+                fs.mkdirSync(part);
+            }
+        });
     });
 
-    return chain;
+    return this;
 };
--- test/fs-actions/fs-actions.spec.js
+++ test/fs-actions/fs-actions.spec.js
@@ -1,19 +1,46 @@
-var expect = require('chai').expect
+var chai = require('chai')
+    , spies = require('chai-spies');
+
+chai.use(spies);
+
+var expect = chai.expect
+    , _ = require('lodash')
+    , q = require('q')
+    , fs = require('fs')
     , FsActions = require('../../modules/fs-actions');
 
 describe('FsActions', function() {
-    var fs = null;
+    var fsActions = null;
 
     it('should creates a new instance', function() {
-        fs = new FsActions({debug: true});
-        expect(fs).to.be.an.instanceof(FsActions);
-        expect(fs.config().get('fsDelimiter')).to.be.equal('/');
-        expect(fs.config().get('debug')).to.be.equal(true);
+        fsActions = new FsActions({debug: true});
+        expect(fsActions).to.be.an.instanceof(FsActions);
+        expect(fsActions.config().get('fsDelimiter')).to.be.equal('/');
+        expect(fsActions.config().get('debug')).to.be.equal(true);
+        fsActions.config().set('debug', true);
+        expect(q.longStackSupport).to.be.true;
     });
 
-    it('should creates a new directory', function(done) {
-        var dir = __dirname + '/tests';
-        fs.mkdir(dir)
+    it('should creates a new directory', function() {
+        var dir = __dirname + '/tests/1/2/3';
+        fsActions.createDirSync(dir);
+        expect(fs.existsSync(dir)).to.be.true;
+    });
+
+    it('should creates multiple new directories', function() {
+        var dirs = [
+            __dirname + '/tests/2/3/4',
+            __dirname + '/tests/2/4/5'
+        ];
+        fsActions.createDirSync(dirs);
+        _.forEach(function(dir) {
+            expect(fs.existsSync(dir)).to.be.true;
+        });
+    });
+
+    it('should creates a new directory async', function(done) {
+        var dir = __dirname + '/tests/3/4/5';
+        fsActions.createDir(dir)
         .then(function(created) {
             expect(created).to.be.true;
             done();
@@ -24,8 +51,11 @@ describe('FsActions', function() {
     });
 
     it('should creates a new directory with sub-directories', function(done) {
-        var dir = __dirname + '/tests/sub1/sub2/sub3';
-        fs.mkdir(dir)
+        var dirs = [
+            __dirname + '/tests/4/5/6',
+            __dirname + '/tests/4/6/7'
+        ];
+        fsActions.createDir(dirs)
         .then(function(created) {
             expect(created).to.be.true;
             done();
