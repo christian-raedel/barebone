1d98a4ac9714adbdeb2e1544d805a60554e70f15
Christian Rädel <christian.raedel@gmail.com>
2014-07-17 09:02:52 +0200

    added fileTransport to Logger

--- modules/logger/logger.js
+++ modules/logger/logger.js
@@ -2,7 +2,6 @@ var _ = require('lodash')
     , printf = require('util').format
     , fs = require('fs')
     , colors = require('colors')
-    , q = require('q')
     , Conf = require('../conf');
 
 module.exports = Logger;
@@ -17,14 +16,12 @@ function Logger(config) {
 
 Logger.prototype.config = function(config) {
     var def = {
-        transport: 'console',
         theme: {
             info: 'green',
             warn: 'yellow',
             debug: 'grey',
             error: 'redBG'
-        },
-        logfile: null
+        }
     };
 
     if (!config) {
@@ -36,9 +33,7 @@ Logger.prototype.config = function(config) {
     }
 
     var conf = new Conf('Logger', [
-        'transport',
-        'theme',
-        'logfile'
+        'theme'
     ])
     .defaults(def);
 
@@ -47,97 +42,82 @@ Logger.prototype.config = function(config) {
         colors.setTheme(args.newValue);
     });
 
-    conf.on('onValueChanged:transport', function(args) {
-        var logfile;
-
-        switch (args.newValue) {
-            case 'json':
-                logfile = [];
-            break;
-            default:
-                logfile = null;
-        }
-
-        this.logfile = logfile;
-    });
-
-    conf.on('onValueChanged:logfile', function(args) {
-        if (args.oldValue && self.logfile) {
-            self._closefile();
-        }
-
-        var logfile = null;
-        if (args.newValue) {
-            logfile = self._openfile(args.newValue);
-        }
-
-        self.logfile = logfile;
-    });
-
     this.conf = conf.load(config);
     return conf;
 };
 
-Logger.prototype._openfile = function(filename) {
-    var logfile = null;
+Logger.prototype.use = function(fn) {
+    if (!_.isFunction(fn)) {
+        throw new Error('invalid transport for using with logger!');
+    }
 
-    logfile = fs.createWriteStream(filename, {
-        flags: 'a',
-        encoding: 'utf8'
-    });
+    var transports = this.transports || [];
 
-    logfile.on('error', function(err) {
-        throw err;
-    });
+    transports.push(fn);
 
-    logfile.write(this._format('info', 'logfile opened...'));
-
-    return logfile;
+    this.transports = transports;
+    return this;
 };
 
-Logger.prototype._closefile = function() {
-    var defer = q.defer()
-        , logfile = this.logfile;
+Logger.prototype.fileTransport = function(logfile) {
+    var conf = new Conf('fileTransport', ['logfile', 'autoClose'])
+    .defaults({autoClose: 60 * 1000});
 
-    logfile.on('finish', function() {
-        defer.resolve(true);
-    });
+    if (_.isPlainObject(logfile)) {
+        conf = conf.load(logfile);
+    }
 
-    logfile.on('error', function(err) {
-        defer.reject(err);
-    });
+    if (_.isString(logfile)) {
+        conf.set('logfile', logfile);
+    }
 
-    logfile.end(this._format('info', 'logfile closed...'));
-    this.logfile = null;
+    var stream = null
+        , timeout = null;
 
-    return defer.promise;
-};
+    return function(obj) {
+        var message = printf.apply(null, _.flatten([
+            '[%s] %s - %s\n',
+            new Date(obj.timestamp),
+            obj.level,
+            obj.message
+        ]));
 
-Logger.prototype.shutdown = function() {
-    return this._closefile();
+        var logfile = conf.get('logfile');
+
+        if (!stream) {
+            stream = fs.createWriteStream(logfile, {
+                encoding: 'utf8',
+                mode: 0600,
+                flags: 'a'
+            });
+            stream.write('Logger:fileTransport stream opened...\n');
+        } else if (timeout) {
+            cancelTimeout(timeout);
+        }
+
+        stream.write(message);
+
+        timeout = setTimeout(function() {
+            stream.end('Logger:fileTransport stream closed...\n');
+            stream = null;
+        }, conf.get('autoClose'));
+    };
 };
 
 Logger.prototype.log = function(level, message) {
     var args = _.toArray(arguments).slice(2)
         , logfile = this.logfile;
 
-    var jsonObj = {
+    var obj = {
         timestamp: new Date().getTime(),
         level: level,
         message: printf.apply(null, _.flatten([message, args]))
     };
 
-    message = this._format.apply(null, _.flatten([level, message, args]));
-
-    switch (this.conf.get('transport')) {
-        case 'file':
-            logfile.write(message + '\n');
-            break;
-        case 'json':
-            logfile.push(jsonObj);
-        default:
-            console.log(message[level] + '\n');
-    }
+    transports = this.transports || [];
+    _.forEach(transports, function(transport) {
+        transport.apply(this, [obj]);
+    }, this);
 
     return this;
 };
@@ -148,13 +128,4 @@ Logger.prototype.error = function(message) {
     this.log.apply(null, _.flatten(['error', message, args]));
 
     return this;
-};
-
-Logger.prototype._format = function(level, message) {
-    var args = _.toArray(arguments).slice(2);
-
-    var date = new Date().toLocaleTimeString();
-    message = printf.apply(null, _.flatten(['[%s] %s - ' + message, date, level, args]));
-
-    return message;
 };
--- test/unit/logger/logger.spec.js
+++ test/unit/logger/logger.spec.js
@@ -10,29 +10,23 @@ describe('Logger', function() {
     it('should instanciates', function() {
         logger = new Logger();
         expect(logger).to.be.an.instanceof(Logger);
-        expect(logger.config().get('transport')).to.be.equal('console');
         colors.setTheme(logger.config().get('theme'));
     });
 
-    it('should evaluates formatstrings', function() {
-        var message = logger._format('warn', '%sl%s', 'd', 'c');
-        expect(message.substr(message.indexOf('- ') + 2, message.length)).to.be.equal('dlc');
-    });
-
     it('should logs a message to a logfile', function(done) {
+        this.timeout(5000);
+
         var filename = __dirname + '/test.log';
-        logger.config().set('transport', 'file');
-        logger.config().set('logfile', filename);
+        logger.use(logger.fileTransport({
+            logfile: filename,
+            autoClose: 0
+        }));
         logger.log('error', 'test%serror', '123');
-        logger.shutdown().then(function(closed) {
-            expect(closed).to.be.true;
+        setTimeout(function() {
             var data = fs.readFileSync(filename, {encoding: 'utf8'});
             expect(data).to.match(/test123error/);
             fs.unlinkSync(filename);
             done();
-        })
-        .catch(function(reason) {
-            done(reason);
-        });
+        }, 2000);
     });
 });
